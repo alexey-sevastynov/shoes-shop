171833f9dd862d33f5efe80daf754787a848fe89
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-05-18 20:10:50 +0300

    karusel page , logica

--- src/pages/KaruselPage.tsx
+++ src/pages/KaruselPage.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 import arrowLeft from "../assets/image/arrowLeft.png";
 import arrowRight from "../assets/image/arrowRight.png";
-import zoom from "../assets/image/zoom.png";
 import { Link } from "react-router-dom";
-
-const src: string =
-  "https://respect-shoes.com.ua/image/data/products/IS73-153225/IS73-153225_4.jpg";
+import { useAppSelector } from "../redux/hook";
+import { selectorShoesData } from "../redux/slices/shoes";
 
 interface FotoKaruselProps {}
 
 const KaruselPage: React.FC<FotoKaruselProps> = () => {
+  const { currentItem } = useAppSelector(selectorShoesData);
+  const { imageURL } = currentItem;
+
+  const firstFoto = imageURL[0];
+
+  const [id, setId] = React.useState(1);
+  const [src, setSrc] = React.useState(firstFoto);
   const [backgroundImage, setBackgroundImage] = React.useState(`url(${src})`);
   const [backgroundPosition, setBackgroundPosition] = React.useState("0% 0%");
 
@@ -22,11 +27,68 @@ const KaruselPage: React.FC<FotoKaruselProps> = () => {
 
     setBackgroundPosition(`${x}% ${y}%`);
   };
+
+  React.useEffect(() => {
+    setSrc(imageURL[id - 1]);
+    setBackgroundImage(`url(${imageURL[id - 1]})`);
+  }, [id]);
+
+  const onClickImage = (url: string) => {
+    const index = url.indexOf("_");
+    const idSrc = +url[index + 1]; // 0,1,2,3,4,5
+    setSrc(url);
+    setBackgroundImage(`url(${url})`);
+    setId(idSrc);
+  };
+
+  const onClickLeft = () => {
+    if (id < 2) {
+      console.log("<1", id);
+      setId(5);
+      setSrc(imageURL[5]);
+      setBackgroundImage(`url(${imageURL[5]})`);
+    } else {
+      setId((prev) => prev - 1);
+      setBackgroundImage(`url(${imageURL[id - 1]})`);
+    }
+  };
+  const onClickRight = () => {
+    if (id > 4) {
+      setId(1);
+      setSrc(imageURL[1]);
+      setBackgroundImage(`url(${imageURL[1]})`);
+    } else {
+      setId((prev) => prev + 1);
+      setSrc(imageURL[id + 1]);
+      setBackgroundImage(`url(${imageURL[id + 1]})`);
+    }
+  };
+
+  const showShoesFooter = imageURL.map((image, indexId) => {
+    if (indexId + 1 === id) {
+      return (
+        <div className="futer-foto">
+          <img className="active-foto" src={image} alt="foto" />
+          <div className="active" />
+        </div>
+      );
+    }
+    return (
+      <div
+        key={image}
+        className="futer-foto"
+        onClick={() => onClickImage(image)}
+      >
+        <img src={image} alt="foto" />
+      </div>
+    );
+  });
   return (
     <div className="karusel">
       <div className="karusel-main">
         <img
           className="arrow"
+          onClick={onClickLeft}
           src={arrowLeft}
           alt="left"
           height={40}
@@ -48,49 +110,19 @@ const KaruselPage: React.FC<FotoKaruselProps> = () => {
 
         <img
           className="arrow"
+          onClick={onClickRight}
           src={arrowRight}
           alt="left"
           height={40}
           width={40}
         />
         <Link to="/description">
-          <div className="close" />
+          <div className="close-block">
+            <div className="close" />
+          </div>
         </Link>
       </div>
-      <div className="karusel-futer">
-        <div className="futer-foto">
-          <img
-            className="active-foto"
-            src="https://respect-shoes.com.ua/image/cache/data/products/IS73-153225/IS73-153225-1-76x101.jpg"
-            alt="foto"
-          />
-          <div className="active" />
-        </div>
-        <div className="futer-foto">
-          <img
-            src="https://respect-shoes.com.ua/image/cache/data/products/IS73-153225/IS73-153225-1-76x101.jpg"
-            alt="foto"
-          />
-        </div>
-        <div className="futer-foto">
-          <img
-            src="https://respect-shoes.com.ua/image/cache/data/products/IS73-153225/IS73-153225-1-76x101.jpg"
-            alt="foto"
-          />
-        </div>
-        <div className="futer-foto">
-          <img
-            src="https://respect-shoes.com.ua/image/cache/data/products/IS73-153225/IS73-153225-1-76x101.jpg"
-            alt="foto"
-          />
-        </div>
-        <div className="futer-foto">
-          <img
-            src="https://respect-shoes.com.ua/image/cache/data/products/IS73-153225/IS73-153225-1-76x101.jpg"
-            alt="foto"
-          />
-        </div>
-      </div>
+      <div className="karusel-futer">{showShoesFooter}</div>
     </div>
   );
 };
