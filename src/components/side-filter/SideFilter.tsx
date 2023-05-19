cf4aef199e84f33de94683b340db82ebd836df02
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-05-19 20:40:54 +0300

    filters for mobile

--- src/components/side-filter/SideFilter.tsx
+++ src/components/side-filter/SideFilter.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import styles from "./sideFilter.module.scss";
 import PriceSlider from "./PriceSlider";
 import Checkbox from "./Checkbox";
-import { allCategories } from "../../assets/listAllCategories";
 import { useAppDispatch, useAppSelector } from "../../redux/hook";
-import { allColors } from "../../assets/listAllColors";
-import { allSize } from "../../assets/listAllSizes";
-import { allSeasons } from "../../assets/listAllSeasons";
 import { selectTranslations } from "../../redux/slices/i18nSlice";
 import {
   selectorSort,
@@ -15,7 +11,12 @@ import {
   setSizes,
   setTypes,
 } from "../../redux/slices/filterSlice";
-import { selectorShoesData, showByCategory } from "../../redux/slices/shoes";
+import { selectorShoesData } from "../../redux/slices/shoes";
+
+type ShowTransleteItem = {
+  en: string;
+  ua: string;
+};
 
 interface SideFilterProps {
   onClickCategories?: any;
@@ -24,12 +25,16 @@ interface SideFilterProps {
 const SideFilter: React.FC<SideFilterProps> = ({ onClickCategories }) => {
   const dispatch = useAppDispatch();
 
-  // const [sizes, setSizes] = React.useState(allSize);
-  // const [seasons, setSeasons] = React.useState(allSeasons);
-
   const t = useAppSelector(selectTranslations);
   const { lang } = useAppSelector((state) => state.i18n);
   const { types, colors, seasons, sizes } = useAppSelector(selectorSort);
+  const { maxPrice, minPrice } = useAppSelector(selectorShoesData);
+
+  const [toggleCategory, setTogglecategory] = React.useState(false);
+  const [togglePrice, setTogglePrice] = React.useState(false);
+  const [toggleColor, setToggleColor] = React.useState(false);
+  const [toggleSize, setToggleSize] = React.useState(false);
+  const [toggleSeason, setToggleSeason] = React.useState(false);
 
   const onHandChangCategory = (index: number) => {
     dispatch(setTypes(index));
@@ -44,94 +49,158 @@ const SideFilter: React.FC<SideFilterProps> = ({ onClickCategories }) => {
     dispatch(setSeasons(index));
   };
 
+  const showTranslete = (obj: ShowTransleteItem) => {
+    const { ua, en } = obj;
+    return lang === "ua" ? ua : en;
+  };
+
   return (
     <div className={styles.root}>
-      <div className={styles.filterGroup}>
+      <div
+        className={styles.filterGroup}
+        onClick={() => setTogglecategory(!toggleCategory)}
+      >
         <p>{t.sideFilter.category}</p>
+        <div className={styles.filterActive}>
+          {types
+            .filter((category) => category.checked === true)
+            .map((categoryName) => (
+              <p key={categoryName.en}>{showTranslete(categoryName)}</p>
+            ))}
+        </div>
         <span></span>
       </div>
-      <div>
-        {types.map((obj, id) => (
-          <Checkbox
-            key={obj.en}
-            isCheked={obj.checked}
-            checkHandler={() => onHandChangCategory(id)}
-            label={lang === "en" ? obj.en : obj.ua}
-            index={id}
-          />
-        ))}
-        <button className={styles.btn} onClick={onClickCategories}>
-          {t.sideFilter.show}
-        </button>
-      </div>
 
-      <div className={styles.filterGroup}>
+      {toggleCategory && (
+        <div>
+          {types.map((obj, id) => (
+            <Checkbox
+              key={obj.en}
+              isCheked={obj.checked}
+              checkHandler={() => onHandChangCategory(id)}
+              label={lang === "en" ? obj.en : obj.ua}
+              index={id}
+            />
+          ))}
+
+          <button className={styles.btn} onClick={onClickCategories}>
+            {t.sideFilter.show}
+          </button>
+        </div>
+      )}
+      <div
+        className={styles.filterGroup}
+        onClick={() => setTogglePrice(!togglePrice)}
+      >
         <p>{t.sideFilter.price}</p>
+        <div className={styles.filterActive}>
+          <p>от</p>
+          <p className={styles.green}>{minPrice}</p>
+          <p>до</p>
+          <p className={styles.red}>{maxPrice}</p>
+        </div>
         <span></span>
       </div>
-
-      <div>
-        <PriceSlider valutaText={t.sideFilter.uah} />
-        <button className={styles.btn} onClick={onClickCategories}>
-          {t.sideFilter.show}
-        </button>
-      </div>
-      <div className={styles.filterGroup}>
+      {togglePrice && (
+        <div>
+          <PriceSlider valutaText={t.sideFilter.uah} />
+          <button className={styles.btn} onClick={onClickCategories}>
+            {t.sideFilter.show}
+          </button>
+        </div>
+      )}
+      <div
+        className={styles.filterGroup}
+        onClick={() => setToggleColor(!toggleColor)}
+      >
         <p>{t.sideFilter.color}</p>
+        <div className={styles.filterActive}>
+          {colors
+            .filter((color) => color.checked === true)
+            .map((colorName) => (
+              <p key={colorName.en}>{showTranslete(colorName)}</p>
+            ))}
+        </div>
         <span></span>
       </div>
-      <div>
-        {colors.map((obj, id) => (
-          <Checkbox
-            key={obj.en}
-            isCheked={obj.checked}
-            checkHandler={() => onHandChangColor(id)}
-            label={lang === "en" ? obj.en : obj.ua}
-            index={id}
-          />
-        ))}
-        <button className={styles.btn} onClick={onClickCategories}>
-          {t.sideFilter.show}
-        </button>
-      </div>
-      <div className={styles.filterGroup}>
+      {toggleColor && (
+        <div>
+          {colors.map((obj, id) => (
+            <Checkbox
+              key={obj.en}
+              isCheked={obj.checked}
+              checkHandler={() => onHandChangColor(id)}
+              label={lang === "en" ? obj.en : obj.ua}
+              index={id}
+            />
+          ))}
+          <button className={styles.btn} onClick={onClickCategories}>
+            {t.sideFilter.show}
+          </button>
+        </div>
+      )}
+      <div
+        className={styles.filterGroup}
+        onClick={() => setToggleSize(!toggleSize)}
+      >
         <p>{t.sideFilter.size}</p>
+        <div className={styles.filterActive}>
+          {sizes
+            .filter((size) => size.checked === true)
+            .map((sizeName) => (
+              <p key={sizeName.name}>{sizeName.name}</p>
+            ))}
+        </div>
         <span></span>
       </div>
-      <div>
-        {sizes.map((size, id) => (
-          <Checkbox
-            key={size.name}
-            isCheked={size.checked}
-            checkHandler={() => onHandChangSize(id)}
-            label={size.name}
-            index={id}
-          />
-        ))}
+      {toggleSize && (
+        <div>
+          {sizes.map((size, id) => (
+            <Checkbox
+              key={size.name}
+              isCheked={size.checked}
+              checkHandler={() => onHandChangSize(id)}
+              label={size.name}
+              index={id}
+            />
+          ))}
 
-        <button className={styles.btn} onClick={onClickCategories}>
-          {t.sideFilter.show}
-        </button>
-      </div>
-      <div className={styles.filterGroup}>
+          <button className={styles.btn} onClick={onClickCategories}>
+            {t.sideFilter.show}
+          </button>
+        </div>
+      )}
+      <div
+        className={styles.filterGroup}
+        onClick={() => setToggleSeason(!toggleSeason)}
+      >
         <p>{t.sideFilter.season}</p>
+        <div className={styles.filterActive}>
+          {seasons
+            .filter((season) => season.checked === true)
+            .map((seasonName) => (
+              <p key={seasonName.en}>{showTranslete(seasonName)}</p>
+            ))}
+        </div>
         <span></span>
       </div>
-      <div>
-        {seasons.map((obj, id) => (
-          <Checkbox
-            key={obj.en}
-            isCheked={obj.checked}
-            checkHandler={() => onHandChangSeason(id)}
-            label={lang === "en" ? obj.en : obj.ua}
-            index={id}
-          />
-        ))}
+      {toggleSeason && (
+        <div>
+          {seasons.map((obj, id) => (
+            <Checkbox
+              key={obj.en}
+              isCheked={obj.checked}
+              checkHandler={() => onHandChangSeason(id)}
+              label={lang === "en" ? obj.en : obj.ua}
+              index={id}
+            />
+          ))}
 
-        <button className={styles.btn} onClick={onClickCategories}>
-          {t.sideFilter.show}
-        </button>
-      </div>
+          <button className={styles.btn} onClick={onClickCategories}>
+            {t.sideFilter.show}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/FilterMobilePage.tsx
+++ src/pages/FilterMobilePage.tsx
@@ -1,19 +1,81 @@
 import React from "react";
 import SideFilter from "../components/side-filter/SideFilter";
+import { useAppDispatch, useAppSelector } from "../redux/hook";
+import { fetchShoes, selectorShoesData } from "../redux/slices/shoes";
+import { selectorSort } from "../redux/slices/filterSlice";
+import { Link } from "react-router-dom";
 
 interface FilterMobilePageProps {}
 
 const FilterMobilePage: React.FC<FilterMobilePageProps> = () => {
+  const { items, status, maxPrice, minPrice } =
+    useAppSelector(selectorShoesData);
+  const { sort, types, colors, seasons, sizes } = useAppSelector(selectorSort);
+
+  const dispatch = useAppDispatch();
+
+  const apiShoes = async () => {
+    const showSortName = sort.sortProperty.replace("-", "");
+    const ascOrDesc = sort.sortProperty.includes("-") ? "asc" : "desc";
+    const filterByPrice = `priceSale_gte=${minPrice}&priceSale_lte=${maxPrice}`;
+    const showCategories = types
+      .filter((category) => category.checked)
+      .map((item) => "category=" + item.en.toLowerCase() + "&")
+      .join("");
+    const filterByColors = colors
+      .filter((color) => color.checked)
+      .map((item) => "color.en=" + item.en.toLowerCase() + "&")
+      .join("");
+    const filterBySeasons = seasons
+      .filter((season) => season.checked)
+      .map((item) => "season.en=" + item.en.toLowerCase() + "&")
+      .join("");
+
+    const filterBySizes = sizes
+      .filter((size) => size.checked)
+      .map((item) => "sizes_like=" + item.name + "&")
+      .join("");
+
+    dispatch(
+      fetchShoes({
+        showSortName,
+        ascOrDesc,
+        showCategories,
+        filterByPrice,
+        filterByColors,
+        filterBySeasons,
+        filterBySizes,
+      })
+    );
+  };
+
+  React.useEffect(() => {
+    apiShoes();
+  }, [types, colors, sizes, seasons, maxPrice, minPrice]);
+
+  const onClickCategories = () => {
+    apiShoes();
+    window.history.back();
+  };
+  const onClickBack = () => {
+    window.history.back();
+  };
+
   return (
     <div className="filter-mobile">
       <div className="filter-mobile-header">
         <p>filter</p>
-        <div className="header-close" />
+
+        <div className="header-close" onClick={onClickBack} />
       </div>
       <SideFilter />
       <div className="filter-mobile-footer">
-        <button>Смотреть (275) товаров</button>
-        <button>Назад</button>
+        <button onClick={onClickCategories}>
+          Смотреть ({items.length}) товаров
+        </button>
+        <button className="back" onClick={onClickBack}>
+          Назад
+        </button>
       </div>
     </div>
   );
